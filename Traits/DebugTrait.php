a154e81df0657329edb8d8109177c746621ea829
_ <firminchanteux@gmail.com>
2020-04-15 09:09:49 +0200

    add feature debug

---
+++ Traits/DebugTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace nlib\Log\Traits;
+
+// use nlib\Log\Traits\LogTrait;
+
+trait DebugTrait {
+
+    // use LogTrait;
+
+    private $_debug = false;
+    private $_die = false;
+
+    protected function debug(...$debug) : void {
+        if($this->_debug) :
+            var_dump($debug);
+            $logs = [__CLASS__ . '::' . __FUNCTION__ => json_encode($debug)];
+            if(!$this->_die) $this->log($logs); else $this->dlog($logs);
+        endif;
+    }
+
+    public function setDebug(bool $debug, bool $die = false) : self { $this->_debug = $debug; $this->_die = $die; return $this; }
+}
